cda684c29a0328fa04a38607483084e8e9393426
Zacharie Morin <zachariemorin@hotmail.com>
2026-05-11 19:17:33 -0400

    Initial commit, basic springboot project

---
+++ src/main/java/com/centresportifets/athlets_backend/AthletsBackendApplication.java
@@ -0,0 +1,20 @@
+package com.centresportifets.athlets_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AthletsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthletsBackendApplication.class, args);
+	}
+
+	@GetMapping("/")
+    public String helloWorld() {
+        return "Athlet";
+    }
+}
---
+++ src/test/java/com/centresportifets/athlets_backend/AthletsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centresportifets.athlets_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
