f2ffb47d702d632109d53f631c00440ccb54d7a6
zachyjello <zachariemorin@hotmail.com>
2026-05-21 19:48:49 -0400

    Initial push, contains working docker container initialization for database

--- src/test/java/com/centresportifets/athlets_backend/AthletsBackendApplicationTests.java
+++ src/test/java/com/centresportifets/athlets_backend/AthletsBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.centresportifets.athlets_backend;
+// package com.centresportifets.athlets_backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AthletsBackendApplicationTests {
+// @SpringBootTest
+// class AthletsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
