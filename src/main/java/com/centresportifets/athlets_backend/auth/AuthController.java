c9e44614346ea142a0b344240d4d3810cd407bad
Zacharie Morin <zachariemorin@hotmail.com>
2026-05-23 00:42:20 -0400

    Database runs on launch

--- src/main/java/com/centresportifets/athlets_backend/auth/AuthController.java
+++ src/main/java/com/centresportifets/athlets_backend/auth/AuthController.java
@@ -1,5 +1,14 @@
 package com.centresportifets.athlets_backend.auth;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-    
+    @PostMapping("/login")
+    public void loginUser(){
+
+    }
 }
