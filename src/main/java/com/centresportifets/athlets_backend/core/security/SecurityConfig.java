2f0bf9cadc10dc6d47f6639db8636d5a7102f480
Force-quit <miloulune1@gmail.com>
2026-06-02 11:58:34 -0400

    Logout works (tested)

--- src/main/java/com/centresportifets/athlets_backend/core/security/SecurityConfig.java
+++ src/main/java/com/centresportifets/athlets_backend/core/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -13,6 +14,11 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityContextLogoutHandler logoutHandler(){
+		return new SecurityContextLogoutHandler();
 	}
 
 	@Bean
