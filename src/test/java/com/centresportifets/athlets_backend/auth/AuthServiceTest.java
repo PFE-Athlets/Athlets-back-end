6b62b0d473e5d423e52791806b3140ef2c7e302c
Force-quit <74936311+Force-quit@users.noreply.github.com>
2026-06-21 20:50:09 -0400

    Logout feature and unit tests (#2)
    * Initial push, contains working docker container initialization for database
    
    * Working database upload
    
    * Updated readme to account for database consultation
    
    * Database runs on launch
    
    * Couple of additions:
    - Database runs on launching backend
    - Swagger API doc
    - Hot reload
    - Basic auth repository
    
    * MOST CHANGES DONE
    
    * Changed indentation and spotless settings
    
    * Fixed format of pom.xml that was annoying me
    
    * Logout should work (untested)
    
    * Logout works (tested)
    
    * Logout unit tests
    
    * Style cleaning
    
    * Updated readme
    
    * Logout doesn't return anything, just 200 ok
    
    ---------
    
    Co-authored-by: zachyjello <zachariemorin@hotmail.com>

---
+++ src/test/java/com/centresportifets/athlets_backend/auth/AuthServiceTest.java
@@ -0,0 +1,39 @@
+package com.centresportifets.athlets_backend.auth;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+	@Mock
+	private AuthUserRepository userRepository;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@Mock
+	private SecurityContextLogoutHandler logoutHandler;
+
+	@Test
+	void logoutLogic() {
+		AuthService authService = new AuthService(userRepository, passwordEncoder, logoutHandler);
+		Authentication authentication = mock(Authentication.class);
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+
+		authService.logoutUser(authentication, request, response);
+
+		verify(logoutHandler).logout(request, response, authentication);
+	}
+}
