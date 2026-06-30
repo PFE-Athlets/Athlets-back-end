4b3164bcb9b4c631552cae7a999c310299cf3716
Zacharie Morin <zachariemorin@hotmail.com>
2026-06-30 00:11:31 -0400

    Added a coach user to the data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,4 +15,5 @@ INSERT INTO user_account (
 ('M.', 'Ambeault', 'mambeault0@etsmtl.ca', NULL, 'mambeault0', '$2a$10$z5IAiKe5qGL8VdSEutXZA.UbLnugSqxufxEK4H4QQ2k0R6Mdgop7y', 'Active', 1),       -- admin1 (Admin)
 ('E.', 'Laforce', 'elaforce0@etsmtl.ca', NULL, 'elaforce0', '$2a$10$z5IAiKe5qGL8VdSEutXZA.UbLnugSqxufxEK4H4QQ2k0R6Mdgop7y', 'Active', 1),        -- admin1 (Admin)
 ('L.', 'Seguin', 'lseguin0@etsmtl.ca', NULL, 'lseguin0', '$2a$10$z5IAiKe5qGL8VdSEutXZA.UbLnugSqxufxEK4H4QQ2k0R6Mdgop7y', 'Active', 1),         -- admin1 (Admin)
+('C.', 'Oach', 'coach@etsmtl.ca', NULL, 'coach', '$2a$10$z5IAiKe5qGL8VdSEutXZA.UbLnugSqxufxEK4H4QQ2k0R6Mdgop7y', 'Active', 2),         -- admin1 (Coach)
 ('Test', 'User', 'testuser0@etsmtl.ca', NULL, 'testUser0', '$2a$10$zJbaow2rXTObDtnNmcgbdu9.ufmVcTo6JRAypSkTAetF6HoLAkGva', 'Active', 3);          -- password (Athlete)
