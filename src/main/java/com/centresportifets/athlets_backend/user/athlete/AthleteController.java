dee3ace7b16aa28b0fc3b00ec0b215956869dbc0
Zacharie Morin <zachariemorin@hotmail.com>
2026-07-06 20:05:12 -0400

    first commit

--- src/main/java/com/centresportifets/athlets_backend/user/athlete/AthleteController.java
+++ src/main/java/com/centresportifets/athlets_backend/user/athlete/AthleteController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.centresportifets.athlets_backend.user.athlete.dto.AthleteCreateRequest;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Tag(name = "Athlete controller", description = "Handles athlete creation and related operations")
 @RestController
@@ -26,4 +28,12 @@ public class AthleteController {
         athleteService.createAthlete(request, auth);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping
+    public String getAllAthletes(@RequestParam String param) {
+        return new String();
+    }
+
+    @PutMapping("/")
+    
 }
