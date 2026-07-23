06ccbeaea0ceaae103debcf55a38dc36acbe5bce
Force-quit <miloulune1@gmail.com>
2026-07-23 11:04:56 -0400

    Fixed KineTeam delete function

--- src/main/java/com/centresportifets/athlets_backend/user/kine/KineTeamRepository.java
+++ src/main/java/com/centresportifets/athlets_backend/user/kine/KineTeamRepository.java
@@ -3,10 +3,15 @@ package com.centresportifets.athlets_backend.user.kine;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface KineTeamRepository extends JpaRepository<KineTeam, KineTeamId> {
     List<KineTeam> findByTeamId(Long teamId);
     List<KineTeam> findByKineId(Long kineId);
     boolean existsByKineIdAndTeamId(Long kineId, Long teamId);
+
+    @Transactional
+    @Modifying
     void deleteByKineIdAndTeamId(Long kineId, Long teamId);
 }
